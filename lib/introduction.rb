ae06f609dd281ccd0b552076233b6812bbfe1f10
Nsingletary <singletarybush@gmail.com>
2020-11-12 20:52:05 -0500

    commit tree 47e7cb169241317f324cd5cc58fcb6af2cacc41d

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,8 @@
-#lib/introduction# lib/introduction.rb
-
-
 def introduction(name)
-    puts "Hi, my name is #{name}"
-  end
-  
+  puts "Hi, my name is #{name}."
+end
+
 def introduction_with_language(name, language)
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  end
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
